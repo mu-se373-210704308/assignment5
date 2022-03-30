ffa24f7ac5831b19f4899879b7cb8b55c7a2fec2
İrem Kutnay <iremkutnay@gmail.com>
2022-03-30 11:37:48 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/maltepe/assignment5/MainActivity.kt
@@ -0,0 +1,43 @@
+package tr.edu.maltepe.assignment5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.content.Intent
+import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button2)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        // use arrayadapter and define an array
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "Irem", "Yaren", "Eren",
+            "Ahmet", "Raif"
+        )
+        // access the listView from xml file
+        var mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+
+    }
+
+
+    }
+
+
---
+++ app/src/main/java/tr/edu/maltepe/assignment5/NewActivity.java
@@ -0,0 +1,6 @@
+package tr.edu.maltepe.assignment5;
+
+import android.app.Activity;
+
+public class NewActivity extends Activity {
+}
